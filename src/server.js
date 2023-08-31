d2c07ea19ef49bc8c95ff05998fbea7483bcd5eb
Iannacus <ian.rosas@academlo.com>
2023-08-30 18:41:14 -0600

    aklsdj

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 require("dotenv").config();
 
-const PORT = process.env.PORT ?? 9000;
+const PORT = process.env.PORT ?? 3000;
 
 const server = app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puero ${PORT}`);
